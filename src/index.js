1e4279bbb12f2b941d4758c01a7978c465250be8
Mona Gupta <guptamona915@gmail.com>
2024-11-22 14:00:10 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Serevr is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('MONGO db connection failed !!!' , err)
+})
 
 
 
